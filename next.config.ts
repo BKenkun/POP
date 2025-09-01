795dbf1cbbb6982fe04d662cd9a9eb6374aa2d60
Enrique espin cosme <EnriqueEspinCosme@gmail.com>
2025-09-01 08:45:57 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -40,6 +40,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'jointoyou.it',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
   devIndicators: {
