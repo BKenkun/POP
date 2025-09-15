5de27331ef67347fea4172a933ba5e063b78ef32
Enrique espin cosme <EnriqueEspinCosme@gmail.com>
2025-09-15 10:09:25 +0000

    Try fixing this error: `Build Error: Ecmascript file had an error. Error

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,6 +1,7 @@
+'use client';
+
 import { Sidebar, SidebarProvider, SidebarInset } from "@/components/ui/sidebar";
 import AdminSidebar from "./_components/admin-sidebar";
-import { usePathname } from 'next/navigation';
 import { Providers } from "@/context/providers";
 
 // Este es el layout principal para las páginas autenticadas del admin.
