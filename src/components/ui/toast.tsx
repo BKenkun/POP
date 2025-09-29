b23575a21a0839c87a2390535c6f95e38359b33b
Enrique espin cosme <EnriqueEspinCosme@gmail.com>
2025-09-29 08:35:04 +0000

    Cuando el menú móvil está abierto, debe ser el elemento principal y nada

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -16,7 +16,7 @@ const ToastViewport = React.forwardRef<
   <ToastPrimitives.Viewport
     ref={ref}
     className={cn(
-      "fixed bottom-0 left-0 z-[100] flex max-h-screen w-full flex-col p-4 max-w-[380px]",
+      "fixed bottom-0 left-0 z-40 flex max-h-screen w-full flex-col p-4 max-w-[380px]",
       className
     )}
     {...props}
