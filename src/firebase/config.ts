128f1605140dc6c83d2772a0bfa831379d648d03
Enrique espin cosme <EnriqueEspinCosme@gmail.com>
2025-10-09 01:01:17 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "purorush",
+  "appId": "1:870980470663:web:533824f7e6c462a8d0f522",
+  "apiKey": "AIzaSyDF062S49yLIQxxKSR-YUoHagNFAAeAjg4",
+  "authDomain": "purorush.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "870980470663"
+};
