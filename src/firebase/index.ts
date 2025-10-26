601dabbdf811a6cdd643741a6b7961ce6bfc0965
Enrique espin cosme <EnriqueEspinCosme@gmail.com>
2025-10-26 07:05:11 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve './

---
+++ src/firebase/index.ts
@@ -0,0 +1,8 @@
+// This file is being deprecated and its contents are being moved or removed
+// to simplify the Firebase initialization logic and resolve build errors.
+
+// The functionality of these exports is now handled by src/context/providers.tsx
+// and the individual hooks/components that use Firebase directly.
+
+// No-op export to prevent breaking other potential imports, will be cleaned up.
+export {};
