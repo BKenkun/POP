1594b51158d9c34ea7e4fa82167d56e18a764fac
Enrique espin cosme <EnriqueEspinCosme@gmail.com>
2025-11-02 09:50:39 +0000

    Vale como de dificil seria que el boton desaparezca cuando aparece una n

--- src/components/floating-left-buttons.tsx
+++ src/components/floating-left-buttons.tsx
@@ -7,10 +7,12 @@ import { cn } from '@/lib/utils';
 import { useEffect, useState } from 'react';
 import FloatingLanguageButton from './floating-language-button';
 import WelcomePopupLoader from './welcome-popup-loader';
+import { useToast } from '@/hooks/use-toast';
 
 export default function FloatingLeftButtons() {
     const pathname = usePathname();
     const { consent } = useCookieConsent();
+    const { toasts } = useToast();
     const [isClient, setIsClient] = useState(false);
 
     useEffect(() => {
@@ -22,14 +24,19 @@ export default function FloatingLeftButtons() {
     }
 
     const isBannerVisible = isClient && !consent.necessary;
+    const areToastsVisible = toasts.length > 0;
 
     return (
         <div className={cn(
-            "fixed left-6 z-50 flex flex-col items-center gap-4 transition-all duration-300",
+            "fixed left-6 z-40 flex flex-col items-center gap-4 transition-all duration-300",
             isBannerVisible ? "bottom-[72px]" : "bottom-6"
         )}>
-            <WelcomePopupLoader />
-            <FloatingLanguageButton />
+            <div className={cn("transition-opacity duration-300", areToastsVisible && "opacity-0 invisible")}>
+                <WelcomePopupLoader />
+            </div>
+            <div className={cn("transition-opacity duration-300", areToastsVisible && "opacity-0 invisible")}>
+                 <FloatingLanguageButton />
+            </div>
         </div>
     );
 }
