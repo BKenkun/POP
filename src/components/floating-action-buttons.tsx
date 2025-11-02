3d3c45594942d103c65fc6b14cf86153b188fd34
Enrique espin cosme <EnriqueEspinCosme@gmail.com>
2025-11-02 09:44:47 +0000

    Mira estoy viendo que eres incpaaz de mantener una forma de triangulo co

--- src/components/floating-action-buttons.tsx
+++ src/components/floating-action-buttons.tsx
@@ -27,33 +27,14 @@ export default function FloatingActionButtons() {
 
     return (
         <div className={cn(
-            "fixed right-6 z-50 transition-all duration-300",
+            "fixed right-6 z-50 flex flex-col items-center gap-4 transition-all duration-300",
             isBannerVisible ? "bottom-[72px]" : "bottom-6"
         )}>
-            {/* 
-              Contenedor relativo para posicionar los botones.
-              Las dimensiones (h-40 w-36) definen el "lienzo" sobre el que se colocan los botones.
-            */}
-            <div className="relative h-40 w-36">
-                {/* Botón de CBD (punta izquierda del triángulo) */}
-                <div className="absolute top-1/2 left-0 -translate-y-1/2">
-                    <FloatingCbdButton />
-                </div>
-
-                {/* Botón de Cuenta (esquina superior derecha) */}
-                <div className="absolute top-0 right-0">
-                    <FloatingAccountButton />
-                </div>
-
-                {/* Botón de Carrito (esquina inferior derecha) */}
-                <div className="absolute bottom-0 right-0">
-                    <FloatingCartButton />
-                </div>
-
-                {/* Botón de Idioma (posición central-inferior, más cerca del grupo) */}
-                <div className="absolute bottom-4 left-10">
-                    <FloatingLanguageButton />
-                </div>
+            <FloatingAccountButton />
+            <FloatingCbdButton />
+            <FloatingCartButton />
+            <div className="pt-2">
+                <FloatingLanguageButton />
             </div>
         </div>
     );
